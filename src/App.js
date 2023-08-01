269d8c831759ba634dc389cf5a00a91feddd7558
pankaj <pankajkr12022001@gmail.com>
2023-08-01 10:50:34 +0530

    header page is responsive now

--- src/App.js
+++ src/App.js
@@ -1,16 +1,16 @@
 import './App.css';
-import Checkout from './Component/Checkout';
-import DonationPopUp from './Component/DonationPopUp';
+// import Checkout from './Component/Checkout';
+// import DonationPopUp from './Component/DonationPopUp';
 // import Donation from './Component/Donation';
 // import Checkout from './Component/Checkout';
-// import LoginPage from './Component/LoginPage';
+import LoginPage from './Component/LoginPage';
 // import Signup from './Signup';
 
 
 function App() {
   return (
     <div className="App">
-       <DonationPopUp/>
+       <LoginPage/>
     </div>
   );
 }
